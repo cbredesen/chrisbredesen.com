14caf3c34af5ee713705af92f9334cfd021a8733
Chris Bredesen <chris@breddy.net>
2019-01-20 11:02:41 -0500

    deploy.sh improvements (but it still sucks)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,16 @@
-aws s3 sync public/ s3://chrisbredesen.com
-aws cloudfront create-invalidation --distribution-id E3MIM4G6ZCVGI8 --paths "/*"
+#!/bin/bash
+
+PATHS='/index.html'
+FOLDER='public/'
+BUCKET='chrisbredesen.com'
+
+# look for --invalidate-all (just kidding this is ugly af)
+if [ "$1" ]; then
+    PATHS='/*'
+fi
+
+echo Syncing $FOLDER to bucket $BUCKET
+aws s3 sync $FOLDER s3://$BUCKET
+
+echo Creating CloudFront invalidation on paths $PATHS
+aws cloudfront create-invalidation --distribution-id E3MIM4G6ZCVGI8 --paths $PATHS
